bdb02aae45c8641d01e0391ccda46119c069e188
errr1n <eteply@telus.net>
2024-11-27 15:49:22 -0800

    terrain scan

---
+++ GameOff2024/Assets/Scripts/TerrainScanner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainScanner : MonoBehaviour
+{
+    public GameObject TerrainScannerPrefab;
+    public float duration = 10;
+    public float size = 500;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            SpawnTerrainScanner();
+        }
+    }
+
+    void SpawnTerrainScanner()
+    {
+        GameObject terrainScanner = Instantiate(TerrainScannerPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
+        ParticleSystem terrainScannerPS = terrainScanner.transform.GetChild(0).GetComponent<ParticleSystem>();
+
+        if(terrainScannerPS != null)
+        {
+            var main = terrainScannerPS.main;
+            main.startLifetime = duration;
+            main.startSize = size;
+        }
+        else
+        {
+            Debug.Log("The first child doesn't have a particle system");
+        }
+
+        Destroy(terrainScanner, duration+1);
+    }
+}
